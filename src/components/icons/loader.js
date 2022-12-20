6ebcf49ae2afe0f1dc730a7002b5cee551d9c1df
devanandgalande <devanand.galande@infosys.com>
2022-12-20 10:56:36 +0530

    Initial commit

---
+++ src/components/icons/loader.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const IconLoader = () => (
+
+    
+  <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+    <title>Loader Logo</title>
+    <g>
+      <g id="A" transform="translate(38.000000, 34.000000)">
+        <path
+          d="M17.37 17.07H6.57L4.24 24H3.01l8.23-24h1.52l8.23 24h-1.3zm-.39-1.13-5-14.96-5.03 14.98h10.03z"
+          fill="currentColor"
+        />
+      </g>
+      <path
+        stroke="currentColor"
+        strokeWidth="5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M 50, 5
+                  L 11, 27
+                  L 11, 72
+                  L 50, 95
+                  L 89, 73
+                  L 89, 28 z"
+      />
+    </g>
+  </svg>
+);
+
+export default IconLoader;
+
+
---
+++ src/components/icons/logo.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const IconLogo = () => (
+  <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
+    <title>Logo</title>
+    <g transform="translate(-2.000000, 1.000000)">
+      
+        <polygon
+          id="Shape"
+          stroke="currentColor"
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          points="39 0 0 22 0 67 39 90 78 68 78 23"
+        />
+      </g>
+      <g transform="translate(24.000000, 30.000000)">
+      <path
+          d="M17.37 17.07H6.57L4.24 24H3.01l8.23-24h1.52l8.23 24h-1.3zm-.39-1.13-5-14.96-5.03 14.98h10.03z"
+          fill="currentColor"
+        />
+    </g>
+  </svg>
+);
+
+export default IconLogo;
